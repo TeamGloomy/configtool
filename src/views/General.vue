ba40aec68c39b7c5f7a0926cc9169cc06c7bc1b5
Jay <github@jayuk.org>
2023-01-22 11:22:13 +0000

    3.4.0+5 added fly-e3-v2, fixed random SPI pins in board.txt

--- src/views/General.vue
+++ src/views/General.vue
@@ -13,6 +13,7 @@
 							<option value="fly_cdyv2">Fly-CDYv2</option>
 							<option value="fly_cdyv3">Fly-CDYv3</option>
 							<option value="fly_E3">Fly-E3</option>
+							<option value="fly_e3_v2">Fly-E3-v2</option>
 							<option value="fly_e3_pro">Fly-E3-Pro</option>
 							<option value="fly_e3_prov3">Fly-E3-Pro-v3</option>
 							<option value="fly_F407ZG">Fly-F407ZG</option>
@@ -20,7 +21,8 @@
 							<option value="fly_geminiv1.1">Fly-Gemini-V1.1</option>
 							<option value="fly_geminiv2.0">Fly-Gemini-V2.0</option>
 							<option value="fly_super8">Fly-Super8</option>
-							<option value="fly_super8h7">Fly-Super8-Pro</option>
+							<option value="fly_super8h7">Fly-Super8-Pro H743</option>
+							<option value="fly_super8h7">Fly-Super8-Pro H723</option>
 							<option value="fysetc_spider">Fysetc Spider 407 Version</option>
 							<option value="mkssbase_1.3">MKS SBase 1.3</option>
 							<option value="mkssgenl_1.0">MKS SGen L v1.0</option>
@@ -34,7 +36,9 @@
 							<option value="biquskr_1.3">SKR v1.3</option>
 							<option value="biquskr_1.4">SKR v1.4</option>
 							<option value="biquskr_2">SKR v2.0</option>
-							<option value="biquskr_3">SKR v3/SKR v3 EZ</option>
+							<option value="biquskr_3">SKR v3 H743</option>#
+							<option value="biquskr_3_ez">SKR v3 EZ</option>
+							<option value="biquskr_3_h723">SKR v3 H723</option>
 							<option value="smoothieboard">Smoothieboard</option>
 							<!--option value="duet085">Duet 0.8.5</option>
 							<option value="duetwifi10">Duet 2 WiFi</option>
